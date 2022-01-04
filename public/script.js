117b7d6131a720392846039c220c06f3ab061070
LagunPavel <ALSIN76as@mail.ru>
2022-01-04 15:04:36 +0300

    Add block 9
    Add slider of feedback, also add slick folder and update js file

---
+++ public/script.js
@@ -0,0 +1,16 @@
+/*Slider of feedbacks*/
+
+$(".slider-1").on('afterChange', function (event, slick, currentSlide) {
+    $(".slide-num-current").text("0" + String(currentSlide + 1));
+  });
+  $(document).ready(function () {
+    $('.slider-1').slick({
+      dots: false,
+      prevArrow: $('.pr'),
+      nextArrow: $('.nx'),
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+  
+    });
+  });
